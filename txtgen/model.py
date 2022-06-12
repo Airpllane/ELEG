b549c49c8799b9d20c7676519c66b3e133811c1b
Airpllane <42839732+Airpllane@users.noreply.github.com>
2022-06-12 10:53:03 +0500

    Update model.py

--- txtgen/model.py
+++ txtgen/model.py
@@ -37,37 +37,3 @@ class CharRNN(nn.Module):
             return (torch.zeros(self.n_layers, batch_size, self.hidden_size),
                     torch.zeros(self.n_layers, batch_size, self.hidden_size))
         return torch.zeros(self.n_layers, batch_size, self.hidden_size)
-
-'''
-class CharCNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, vocab = 95, n_layers = 1):
-        super(CharRNN, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.n_layers = n_layers
-        
-        self.encoder = nn.Embedding(vocab, hidden_size)
-        self.cnn = nn.Conv1d(input_size, int(input_size / 2), 3, padding = 1)
-        
-        self.decoder = nn.Linear(int(input_size / 2) * hidden_size, vocab)
-    
-    def forward(self, input, hidden):
-        batch_size = input.size(0)
-        encoded = self.encoder(input)
-        output, hidden = self.rnn(encoded.view(1, batch_size, -1), hidden)
-        output = self.decoder(output.view(batch_size, -1))
-        return output, hidden
-    
-    def forward2(self, input, hidden):
-        encoded = self.encoder(input.view(1, -1))
-        output, hidden = self.rnn(encoded.view(1, 1, -1), hidden)
-        output = self.decoder(output.view(1, -1))
-        return output, hidden
-    
-    def init_hidden(self, batch_size):
-        if self.model_type == 'lstm':
-            return (torch.zeros(self.n_layers, batch_size, self.hidden_size),
-                    torch.zeros(self.n_layers, batch_size, self.hidden_size))
-        return torch.zeros(self.n_layers, batch_size, self.hidden_size)
-'''
